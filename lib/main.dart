5244a49ac672501dc69b7102e4ee23c5c61acecf
bryann54 <alfredbryann54@gmail.com>
2025-07-21 14:36:04 +0300

    account screen refactoring,

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
-// main.dart
+// main.dart - Improved status bar implementation
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'package:wakili/common/helpers/app_router.dart';
+import 'package:wakili/common/helpers/system_ui_helper.dart';
 import 'package:wakili/common/notifiers/locale_provider.dart';
 import 'package:wakili/common/widgets/global_bloc_observer.dart';
 import 'package:wakili/core/di/injector.dart';
@@ -10,14 +11,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:wakili/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:wakili/features/chat_history/data/models/chat_conversation.dart';
 import 'package:wakili/features/wakili/data/models/chat_message.dart';
 import 'package:wakili/firebase_options.dart';
-
 import 'package:wakili/features/chat_history/presentation/bloc/chat_history_bloc.dart';
 import 'package:wakili/features/wakili/presentation/bloc/wakili_bloc.dart';
 import 'package:wakili/features/account/presentation/bloc/account_bloc.dart';
@@ -25,6 +24,9 @@ import 'package:wakili/features/overview/presentation/bloc/overview_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Configure system UI early
+  SystemUIHelper.configureSystemUI();
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -43,10 +45,8 @@ void main() async {
   Hive.registerAdapter(ChatConversationAdapter());
   Hive.registerAdapter(ChatMessageAdapter());
 
-  // Configure dependency injection
   await configureDependencies();
 
-  // Initialize locale provider
   final localeProvider = LocaleProvider();
   localeProvider.loadLocale();
 
@@ -92,39 +92,88 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final systemUiOverlayStyle =
-        MediaQuery.of(context).platformBrightness == Brightness.dark
-            ? SystemUiOverlayStyle.light
-            : SystemUiOverlayStyle.dark;
-    return AnnotatedRegion<SystemUiOverlayStyle>(
-      value: systemUiOverlayStyle,
-      child: MaterialApp.router(
-        debugShowCheckedModeBanner: !kReleaseMode,
-        routerConfig: _appRouter.config(),
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-            backgroundColor: Colors.white,
-            selectedItemColor: Colors.blue,
-            unselectedItemColor: Colors.grey,
-            selectedIconTheme: IconThemeData(size: 28),
-            unselectedIconTheme: IconThemeData(size: 24),
-            type: BottomNavigationBarType.fixed,
-          ),
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: !kReleaseMode,
+      routerConfig: _appRouter.config(),
+      theme: _buildLightTheme(),
+      darkTheme: _buildDarkTheme(),
+      themeMode: ThemeMode.system,
+      builder: (context, child) {
+        // This ensures status bar styling updates when theme changes
+        return AnnotatedRegion<SystemUiOverlayStyle>(
+          value: _getSystemUiOverlayStyle(context),
+          child: child!,
+        );
+      },
+    );
+  }
+
+  ThemeData _buildLightTheme() {
+    return ThemeData(
+      brightness: Brightness.light,
+      primarySwatch: Colors.blue,
+      scaffoldBackgroundColor: Colors.white,
+      cardColor: Colors.white,
+      appBarTheme: const AppBarTheme(
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        elevation: 0,
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarColor: Colors.transparent,
+          statusBarIconBrightness: Brightness.dark,
+          statusBarBrightness: Brightness.light,
         ),
-        darkTheme: ThemeData(
-            brightness: Brightness.dark,
-            primarySwatch: Colors.blue,
-            bottomNavigationBarTheme: BottomNavigationBarThemeData(
-              backgroundColor: Colors.grey[900],
-              selectedItemColor: Colors.blue,
-              unselectedItemColor: Colors.grey[500],
-              selectedIconTheme: const IconThemeData(size: 28),
-              unselectedIconTheme: const IconThemeData(size: 24),
-              type: BottomNavigationBarType.fixed,
-            )),
-        themeMode: ThemeMode.light,
       ),
+      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+        backgroundColor: Colors.white,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        selectedIconTheme: IconThemeData(size: 28),
+        unselectedIconTheme: IconThemeData(size: 24),
+        type: BottomNavigationBarType.fixed,
+      ),
+    );
+  }
+
+  ThemeData _buildDarkTheme() {
+    return ThemeData(
+      brightness: Brightness.dark,
+      primarySwatch: Colors.blue,
+      scaffoldBackgroundColor: Colors.grey[900],
+      cardColor: Colors.grey[850],
+      appBarTheme: AppBarTheme(
+        backgroundColor: Colors.grey[900],
+        foregroundColor: Colors.white,
+        elevation: 0,
+        systemOverlayStyle: const SystemUiOverlayStyle(
+          statusBarColor: Colors.transparent,
+          statusBarIconBrightness: Brightness.light,
+          statusBarBrightness: Brightness.dark,
+        ),
+      ),
+      bottomNavigationBarTheme: BottomNavigationBarThemeData(
+        backgroundColor: Colors.grey[900],
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey[500],
+        selectedIconTheme: const IconThemeData(size: 28),
+        unselectedIconTheme: const IconThemeData(size: 24),
+        type: BottomNavigationBarType.fixed,
+      ),
+    );
+  }
+
+  SystemUiOverlayStyle _getSystemUiOverlayStyle(BuildContext context) {
+    final brightness = Theme.of(context).brightness;
+    final isLight = brightness == Brightness.light;
+
+    return SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: isLight ? Brightness.dark : Brightness.light,
+      statusBarBrightness: isLight ? Brightness.light : Brightness.dark,
+      systemNavigationBarColor: isLight ? Colors.white : Colors.grey[900],
+      systemNavigationBarIconBrightness:
+          isLight ? Brightness.dark : Brightness.light,
+      systemNavigationBarDividerColor: Colors.transparent,
     );
   }
 }
